8b42d3603478cb0833c0340732a5ef28b952b905
guriyakumari <guriyakumarigupta551@gmail.com>
2022-06-04 19:05:06 +0530

    tokenforsucessfullylogincandidatewillbecommited

--- keys.js
+++ keys.js
@@ -1,3 +1,4 @@
 module.exports={
-    MONGOURI:"mongodb+srv://Guriyakumari_770:eYaCxxIRkxUJaGAw@cluster0.mlcyk8z.mongodb.net/?retryWrites=true&w=majority"
+    MONGOURI:"mongodb+srv://Guriyakumari_770:eYaCxxIRkxUJaGAw@cluster0.mlcyk8z.mongodb.net/?retryWrites=true&w=majority",
+    JWT_SECRET:"waghiugieuhgkghaughsugha"
 }
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,10 @@ const mongoose=require("mongoose")
 const User=mongoose.model("User")
 const bcrypt=require('bcryptjs')
 
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
+
+
 
 router.get('/',(req,res)=>
 {
@@ -53,36 +57,36 @@ router.post('/signup',(req,res)=>{
        
     })
 })
-router.post('/signin',(req,res)=>
-{
- const{email,password}=req.body
- if(!email||!password)
- {
- res.status(555).json({error:"please add email and password then proceeded"})
- }
- User.findOne({email:email})
- .then(savedUser=>
-    {
+router.post('/signin',(req,res)=> {
+    const {email,password} = req.body
+    if(!email || !password){
+       return res.status(422).json({error:"please add email or password"})
+    }
+    User.findOne({email:email})
+    .then(savedUser=>{
         if(!savedUser)
         {
-            res.status(566).json({error:"INValid email or password"})
+            
+           return res.status(422).json({error:"Invalid Email or password please change them"})
         }
         bcrypt.compare(password,savedUser.password)
-        .then(doMatch=>
-            {
-                if(doMatch)
-                {
-                    res.json({message:"successfully signed in"})
-                }
-                else{
-                    return res.status(422).json({error:"Invalid Email or password"})
-                }
-            })
-            .catch(err=>
-                {
-                    console.log(err)
-                })
+        .then(doMatch=>{
+            if(doMatch){
+                // res.json({message:"successfully signed in"})
+               const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
+               res.json({token})
+            //    const {_id,name,email,followers,following,pic} = savedUser
+            //    res.json({token,user:{_id,name,email,followers,following,pic}})
+            }
+            else{
+                return res.status(422).json({error:"INVALID EMAIL OR PASSWORD"})
+            }
+        })
+        .catch(err=>{
+            console.log(err)
+        })
     })
 })
+
 
 module.exports=router
