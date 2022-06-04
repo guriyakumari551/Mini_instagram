2103779a20cff3fa8b1a80f2efbf69aee5ae1971
guriyakumari <guriyakumarigupta551@gmail.com>
2022-06-04 17:36:28 +0530

    hashpasswordcommited

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express=require("express")
 const router=express.Router();
 const mongoose=require("mongoose")
 const User=mongoose.model("User")
+const bcrypt=require('bcryptjs')
+
 
 router.get('/',(req,res)=>
 {
@@ -18,14 +20,18 @@ router.post('/signup',(req,res)=>{
         if(savedUser){
           return res.status(422).json({error:"user already exists with that email"})
         }
+      
+        bcrypt.hash(password,12)
+      .then(hashedpassword=>{
+            const user = new User({
+                email,
+                password:hashedpassword,
+                name,
+                
+            })
        
-      const  user=new User(
-          {
-              email,
-              password,
-              name
-          }
-      )
+     
+      
               user.save()
               .then(user=>{
                   // transporter.sendMail({
@@ -40,7 +46,12 @@ router.post('/signup',(req,res)=>{
                   console.log(err)
               })
         })
+        .catch(err=>
+            {
+                console.log(err)
+            })
        
     })
+})
 
-module.exports=router;
+module.exports=router
