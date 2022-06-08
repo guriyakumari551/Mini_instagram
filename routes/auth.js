fa4fdee28d8b25fb52f4dddf0ffbb965e692a23d
guriyakumari <guriyakumarigupta551@gmail.com>
2022-06-08 16:32:01 +0530

    commited

--- routes/auth.js
+++ routes/auth.js
@@ -78,7 +78,9 @@ router.post('/signin',(req,res)=> {
             if(doMatch){
                
                const token = jwt.sign({_id:savedUser._id},JWT_SECRET)
-               res.json({token})
+               res.json({token,user:{}})
+               const {_id,name,email}=savedUser
+               res.json({token,user:{_id,name,email}})
           
             }
             else{
